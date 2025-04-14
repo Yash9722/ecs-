2b177b4e202c497a4f846cf0adb53d6c4fb2a386
Yash9722 <132436407+Yash9722@users.noreply.github.com>
2025-04-14 15:08:43 +0530

    Update main.go

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello from vpc endpoint!")
+    fmt.Fprintln(w, "Hello from vpc endpoint! its working")
 }
 
 func main() {
